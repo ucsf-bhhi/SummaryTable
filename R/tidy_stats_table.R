db9480169c0a8bcdff42b778ecc6171fb6474dd2
SaraMColomBrana <sara.colom@ucsf.edu>
2023-07-11 10:28:18 -0700

    add a tidy_stats_table function

---
+++ R/tidy_stats_table.R
@@ -0,0 +1,36 @@
+tidy_stats_var <- function(stats_table, var_to_tidy){
+
+  Incorp_label <- stats_table %>%
+    slice(1) %>%
+    pull({{var_to_tidy}})
+
+  stats_table %>%
+    mutate("{{var_to_tidy}}" := c(Incorp_label, rep("", n()-1)))
+
+
+}
+
+# Tidy var_summarise output
+#' Generate table with summary statistics
+#'
+#' @importFrom magrittr %>%
+#' @param dat stats table output from `var_summarise()`
+#'
+#' @return Stats output field_label tidydied
+#' @export
+#'
+#' @examples SummaryTable::var_summarise(iris, var = "Petal.Length", treatment = "Species") %>% tidy_stats_table()
+#'
+
+
+tidy_stats_table <- function(stats_table, var){
+
+  stats_table %>%
+    group_by(Variable) %>%
+    group_modify(
+      ~tidy_stats_var(.x, var)
+    ) %>%
+    ungroup()
+
+
+}
